030065fc16904d75a454242e2f8ca0f24dc454f6
unknown <Tyler@.(none)>
2010-12-02 19:21:13 -0700

    Chaning the connection

--- App_Code/CommonPage.cs
+++ App_Code/CommonPage.cs
@@ -5,7 +5,7 @@ public class CommonPage
 {
     public SqlConnection GetConnection()
     {
-        SqlConnection myConn = new SqlConnection("Data Source=premsql2e.brinkster.com;Initial Catalog=aphramzo;user id=aphramzo;password=whyme111;Transaction Binding=Explicit Unbind;MultipleActiveResultSets=true");
+        SqlConnection myConn = new SqlConnection("Data Source=premsql2e.brinkster.com;Initial Catalog=aphramzo;user id=aphramzo;password=Ava1anch3;Transaction Binding=Explicit Unbind;MultipleActiveResultSets=true");
         myConn.Open();
         return myConn;
     }
