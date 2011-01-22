277e2e68b8998f2ee145550cc5f6dc46db660d0d
Tyler Walborn <im.t.wal@gmail.com>
2011-01-21 19:58:05 -0700

    Adding DB Backup

--- js/PlayerCompare.js
+++ js/PlayerCompare.js
@@ -6,7 +6,7 @@
             return;
         }
         $('#compareChart').insertFusionCharts({
-            swfPath: "../AnAvalancheOfStats/charts/",
+            swfPath: "../charts/",
             type: "MSLine2D",
             data: "PlayerCompareChart.aspx?Player1Id=" + $('#ctl00_mainBody_player1').val() + "&Player2Id=" + $('#ctl00_mainBody_player2').val() + "&stat=" + $('#ctl00_mainBody_stat').val(),
             dataFormat: "URIData",
@@ -18,7 +18,7 @@
 
     if ($('#ctl00_mainBody_player1').val() != '' || $('#ctl00_mainBody_player1').val() != '') {
         $('#compareChart').insertFusionCharts({
-            swfPath: "../AnAvalancheOfStats/charts/",
+            swfPath: "../charts/",
             type: "MSLine2D",
             data: "PlayerCompareChart.aspx?Player1Id=" + $('#ctl00_mainBody_player1').val() + "&Player2Id=" + $('#ctl00_mainBody_player2').val() + "&stat=" + $('#ctl00_mainBody_stat').val(),
             dataFormat: "URIData",
@@ -30,7 +30,7 @@
 
     $('#createLink').click(function(event) {
         event.preventDefault();
-        var link = 'http://anavalancheofstats.com/PlayerCompare.aspx?player1Id=' + $('#ctl00_mainBody_player1').val() + '&player2Id=' + $('#ctl00_mainBody_player2').val() + '&stat=' + $('#ctl00_mainBody_stat').val();
+        var link = 'http:/www.AnAvalancheOfStats.com/PlayerCompare.aspx?player1Id=' + $('#ctl00_mainBody_player1').val() + '&player2Id=' + $('#ctl00_mainBody_player2').val() + '&stat=' + $('#ctl00_mainBody_stat').val();
         $('#linkSpan').text(link);
     });
 });
